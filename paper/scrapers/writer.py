30582c7aabe9786a74a6d24bf17c1773320b16b3
Cine-Pixel <ikaumphriani@gmail.com>
2024-04-10 18:54:23 +0400

    update log level

--- paper/scrapers/writer.py
+++ paper/scrapers/writer.py
@@ -5,7 +5,7 @@ import pandas as pd
 from scrapers.scraper import Scraper, DIRECTORY
 from scrapers.ss_scraper import SSScraper
 
-logging.basicConfig(filename="logs.log", filemode="a", level=logging.DEBUG)
+logging.basicConfig(filename="logs.log", filemode="a", level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
